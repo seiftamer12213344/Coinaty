4a55d6aaf50d6d75f9c40d7fe89d99c03ed52266
seiftamer12213344 <seiftamer12213344@gmail.com>
2026-05-06 03:31:58 +0000

    pushed code to github

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,26 +1,37 @@
-{pkgs}: {
+{ pkgs, ... }: {
+  # Which version of the Nixpkgs channel to use
   channel = "stable-24.05";
+
+  # The packages to install in your environment
   packages = [
-    pkgs.nodejs_20
+    pkgs.nodejs_22
+    pkgs.postgresql
   ];
-  idx.extensions = [
-    "svelte.svelte-vscode"
-    "vue.volar"
-  ];
-  idx.previews = {
+
+  # IDX-specific configuration
+  idx = {
+    # Search for and install VS Code extensions
+    extensions = [
+      "dbaeumer.vscode-eslint"
+    ];
+
+    # Configure the preview environment
     previews = {
-      web = {
-        command = [
-          "npm"
-          "run"
-          "dev"
-          "--"
-          "--port"
-          "$PORT"
-          "--host"
-          "0.0.0.0"
-        ];
-        manager = "web";
+      enable = true; # Often required to initialize the preview block
+      previews = {
+        web = {
+          command = [
+            "npm"
+            "run"
+            "dev"
+            "--"
+            "--port"
+            "$PORT"
+            "--host"
+            "0.0.0.0"
+          ];
+          manager = "web";
+        };
       };
     };
   };
